c4b7a53be30742a8b30e581e262aaf6891e4a257
Pedro Cordeiro <pncordeiro@Pedros-Air.lan>
2017-03-17 13:20:16 +0100

    Enemy vision bug fixed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Enemy : MovingObject {
 
-	public int visionDistance = 4;
-	public LayerMask floorLayerMask;
+	public int visionDistance;
+	public LayerMask safeLayerMask;
 
-
-	private void ChangeFloorTags(Vector2 currentPosition)
+	// The enemy will look at his surroundings
+	// In practice, this will change the layer masks of all non-blocking objects within its vision to 'DangerFloor'
+	private void StayAlert(Vector2 currentPosition)
 	{
 		RaycastHit2D hit;
+
 		boxCollider.enabled = false;
-		for (int i = 1; i <= visionDistance; i++) {
+		for (int i = 0; i <= visionDistance; i++) {
 			Vector2 endPosition = new Vector2 (currentPosition.x, currentPosition.y - i);
-			hit = Physics2D.Linecast (currentPosition, endPosition, floorLayerMask);
+			hit = Physics2D.Linecast (currentPosition, endPosition, safeLayerMask);
+
+			if (hit.transform.gameObject.layer == LayerMask.NameToLayer ("BlockingLayer")) {
+				return;
+			}
+
 			hit.transform.gameObject.layer = LayerMask.NameToLayer ("DangerFloor");
 		}
 		boxCollider.enabled = true;
@@ -24,6 +31,6 @@ public class Enemy : MovingObject {
 	protected override void Start () {
 		base.Start ();
 
-		ChangeFloorTags (transform.position);
+		StayAlert (transform.position);
 	}
 }
